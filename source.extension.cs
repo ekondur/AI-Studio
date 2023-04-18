e75442365f499d5b94472228a1f0463ceb084c9c
Emrah KONDUR <emrahkondur@hotmail.com>
2023-04-18 23:57:34 +0300

    Updated Product ID

--- source.extension.cs
+++ source.extension.cs
@@ -7,12 +7,12 @@ namespace AI_Studio
 {
     internal sealed partial class Vsix
     {
-        public const string Id = "AI_Studio.8fa1980c-d304-499f-8faa-8f0623f4431b";
+        public const string Id = "AI_Studio.74F4FED3-20F1-4F08-BEC5-1219C8EDC2AD";
         public const string Name = "AI Studio";
         public const string Description = @"AI Studio helps you with the power of chatGPT in many subjects such as adding unit tests, refactoring code, adding summary, etc. while writing code, just by right click on the code.";
         public const string Language = "en-US";
         public const string Version = "1.0";
         public const string Author = "Emrah Kondur";
-        public const string Tags = "AI, chatGPT, UnitTest, Refactor, Summary";
+        public const string Tags = "AI, chatGPT, UnitTest, Refactor, Summary, OpenAI";
     }
 }
