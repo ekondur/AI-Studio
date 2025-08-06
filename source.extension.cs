f15dc5b7effe3824f1a3ecee1f8cc82c5b5ce2c0
Emrah Kondur <emrah.kondur@invicti.com>
2025-08-06 10:15:05 +0300

    updated version

--- source.extension.cs
+++ source.extension.cs
@@ -9,10 +9,10 @@ namespace AI_Studio
     {
         public const string Id = "AI_Studio.74F4FED3-20F1-4F08-BEC5-1219C8EDC2AD";
         public const string Name = "AI Studio";
-        public const string Description = @"AI Studio helps you with the power of chatGPT in many subjects such as adding unit tests, refactoring code, adding summary, etc. while writing code, just by right click on the code.";
+        public const string Description = @"Supercharge your Visual Studio workflow with AI-powered tools like code generation, refactoring, commenting, unit test creation, and more, right from the right-click menu.";
         public const string Language = "en-US";
-        public const string Version = "1.6.0";
+        public const string Version = "2.0.0";
         public const string Author = "Emrah Kondur";
-        public const string Tags = "AI, chatGPT, UnitTest, Refactor, Summary, OpenAI";
+        public const string Tags = "AI, UnitTest, Refactor, Summary, OpenAI";
     }
 }
